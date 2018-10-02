8790bdb51c3eea97638d9816f8d43f7218e7fc20
Frankline kebei <temkebei@gmail.com>
2018-10-02 16:50:36 -0500

    stockSearch

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
+$(document).ready(function () {
+  const validationList = [];
+  const stocks = [];
 
-$('#btnSubmit').on('click',function(event){
-    event.preventDefault();
-    let stock =$('#searchStock').val();
-    console.log(stock);
-
-$.ajax({
-    url:`https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news&range=1m&last=1`,
-    method:'GET'
-}).then(function(response){
-    console.log(response);
-})
-
-})
-
-// Initial array of stocks
-const stocks = ['FB', "AAPL", 'TSLA', 'GOOG'];
-
-// Function for displaying stock data
-const render = function () {
-
-  // Delete the content inside the stocks-view div prior to adding new stocks
-  // (this is necessary otherwise you will have repeat buttons)
-  $("#stocks-view").empty()
-  // Loop through the array of stocks, then generate buttons for each stock in the array
-  for (let i = 0; i < stocks.length; i++) {
-    $("#stocks-view").append(`<button class="get-stock" value=${stocks[i]}>${stocks[i]}</button>`)
-  }
-
-}
-
-// This function handles events where the add stock button is clicked
-const addButton = function (event) {
-
-  // event.preventDefault() prevents submit button from trying to send a form.
-  // Using a submit button instead of a regular button allows the user to hit
-  // 'Enter' instead of clicking the button if desired
-  event.preventDefault();
-
-  // Write code to grab the text the user types into the input field
-  let newStock = $("#stock-input").val().trim()
-  // Write code to add the new stock into the stocks array
-  stocks.push(newStock);
-  // Write code to delete the contents of the former input
-  $("#stock-input").val("")
-  // The renderButtons function is called, rendering the list of stock buttons
-  render();
-}
-
-$('#add-stock').on('click', addButton);
-$("#stocks-view").on("click",".get-stock", function (event) {
-
-  event.preventDefault();
-  const stock = $(this).val();
-  const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=quote,news&range=1m&last=10`;
-  console.log(stock);
-  console.log(queryURL);
+  const queryAllSymbols = `https://api.iextrading.com/1.0/ref-data/symbols`;
   $.ajax({
-    url:queryURL,
-    method:"GET"
-  }).then(function(response){
+    url: queryAllSymbols,
+    method: 'GET'
+  }).then(function (response) {
+    for (let i = 0; i < response.length; i++) {
+      let validList = response[i].symbol
+      validationList.push(validList);
+    };
+  });
 
-    console.log(response);
+  $('#btnSubmit').on('click', function () {
+    event.preventDefault();
+    let newStock = $('#stockInfo').val().toUpperCase();
+    if (!validationList.includes(newStock)) {
+      alert("Please input a valid stock symbol.");
+    } else {
+      stocks.push(newStock);
+      $('#stockInfo').val('');
+      render();
+    };
+  });
 
-  })
+  $('#btnClear').on('click', function () {
+    $('#tBody').empty();
+    $('#logoDiv').empty();
+    $('#newsDiv').empty();
+  });
 
+  $('#resetButtons').on('click', function () {
+    $('#stocks-view').empty();
+    stocks.length = 0;
+  });
 
-})
-// Calling the renderButtons function to display the initial list of stocks
-render();
+  $("#stocks-view").on("click", "input", function () {
+    let stockSymbol = this.value;
+    const queryURL1 = `https://api.iextrading.com/1.0/stock/${stockSymbol}/quote`;
+    const queryURL2 = `https://api.iextrading.com/1.0/stock/${stockSymbol}/logo`;
+    const queryURL3 = `https://api.iextrading.com/1.0/stock/${stockSymbol}/news`;
 
+    $.ajax({
+      url: queryURL1,
+      method: 'GET'
+    }).then(function (response) {
+      const Q1 = $('#tBody').append(`<tr><td>${response.companyName}</td><td>${response.symbol}</td><td>${response.latestPrice}</td></tr>`);
+    });
+    $.ajax({
+      url: queryURL2,
+      method: 'GET'
+    }).then(function (response) {
+      const Q2 = $('#logoDiv').append(`<img class="image" src="${response.url}">`);
+    });
+    $.ajax({
+      url: queryURL3,
+      method: 'GET'
+    }).then(function (response) {
+      for (let i = 0; i < response.length; i++) {
+        const Q3 = $('#newsDiv').append(`<h5><a href='${response[i].url}'>${response[i].headline}</a></h5><br><p>${response[i].source}</p><br><p>${response[i].datetime}</p><br><p>${response[i].summary}</p><hr>`)
+      };
+    });
+    $('#tBody').empty();
+    $('#logoDiv').empty();
+    $('#newsDiv').empty();
+  });
 
+  const render = function () {
+    $('#stocks-view').empty();
+    for (let i = 0; i < stocks.length; i++) {
+      let theId = 'getStocks' + i;
+      $('#stocks-view').append(`<input id='${theId}' type='button' value='${stocks[i]}'/>&nbsp`);
+    };
+  };
+});
